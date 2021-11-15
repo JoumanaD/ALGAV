93f237646b9b1c9225bb8ea0a114756377523bc3
Joumana <76997880+JoumanaD@users.noreply.github.com>
2021-11-15 20:05:18 +0100

    echauffement

--- ELDAKAR_ZHANG_ALGAV/src/echauffement/Table.java
+++ ELDAKAR_ZHANG_ALGAV/src/echauffement/Table.java
@@ -1,5 +1,25 @@
 package echauffement;
 
-public class Table {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Table {
+	public Table(int x, int n) {
+		Decomposition xb = new Decomposition(x);
+		List<Boolean> lb = xb.getL();
+		Completion tb = new Completion(lb,n);
+		this.l = tb.getLC();
+	}
+	
+	private List<Boolean> l = new ArrayList<Boolean>();
+
+	public List<Boolean> getTableVerite(){
+		return l;
+	}
+	
+	public static void main(String[] args) {
+		Table TableVerite = new Table(38, 16);
+		System.out.println(TableVerite.getTableVerite());
+	  }
+	
 }
