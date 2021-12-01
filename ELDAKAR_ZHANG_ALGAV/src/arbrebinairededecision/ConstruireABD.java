4458776629fb6e5fc5b40c74d91f23f66b4e7079
Joumana <joumana.el-dakar@etu.univ-grenoble-alpes.fr>
2021-12-01 23:23:06 +0100

    cons_arbre

--- ELDAKAR_ZHANG_ALGAV/src/arbrebinairededecision/ConstruireABD.java
+++ ELDAKAR_ZHANG_ALGAV/src/arbrebinairededecision/ConstruireABD.java
@@ -7,7 +7,8 @@ public class ConstruireABD {
     public ConstruireABD (ArrayList<Boolean> T){
         this.T = T;
     }
-
+    
+    /*
     public ArrayList<Integer> transformLbooltoLint(ArrayList<Boolean> T){
         ArrayList<Integer> Tint = new ArrayList<Integer>();
         for(int i =0; i< T.size(); i++){
@@ -17,10 +18,56 @@ public class ConstruireABD {
         return Tint;
 
     }
-    
+    */
+
+    public ABR<Integer> cons_arbre0 () {
+        ABR<Integer> X3 = new ABR<Integer>(3);
+        ABR<Integer> X2 = new ABR<Integer>(2);
+        ABR<Integer> X1 = new ABR<Integer>(1);
+        
+        X2.setGauche(X1);
+        X2.setDroit(X1);
+        X3.setGauche(X2);
+        X3.setDroit(X2);
+    }
+
     public ABR cons_arbre(){
-        ArrayList<Integer> Ta = transformLbooltoLint(this.T);
-        for()
+        double i = 2;
+        double j = this.T.size();
+        ArrayList<ABR> feuilleList = new ArrayList<>();
+        ArrayList<ABR> tempNode = new ArrayList<>();
+        ABR nodeRacine = null;
+
+        int puissanceRacine = (int)(Math.log(j)/Math.log(i)) ;
+        
+        
+        for(int index = 0; index<j;index++){
+            if(this.T.get(index)==true){
+                ABR feuille = new ABR(1);
+                feuilleList.add(feuille);
+            }else{
+                ABR feuille = new ABR(0);
+                feuilleList.add(feuille);
+            }
+        }
+
+        int start;
+        int feuilleIndex;
+        for(int first = puissanceRacine-1; i >= 0; i++){
+            start = 0;
+            feuilleIndex = 0;
+            for(int second = 0; second < Math.pow(2,first); second++){
+                ABR nodeIntern = new ABR(puissanceRacine-first,feuilleList.get(start),feuilleList.get(start+1));
+                feuilleList.set(feuilleIndex,nodeIntern);
+                feuilleIndex = feuilleIndex + 1;
+                start = start + 2;
+                nodeRacine = nodeIntern;
+               
+            }
+        }
+        return nodeRacine;
 
     }
+
+        
 }
